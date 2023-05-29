caf7dfefc81e4a1ca05aca17d89540d51e38856d
kokzhen <22014093@myrp.edu.sg>
2023-05-29 15:13:21 +0800

    first commit

--- P00/src/HelloWorld.java
+++ P00/src/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("My First Java Program");
-
+		System.out.println("Hello world");
 	}
 
 }
